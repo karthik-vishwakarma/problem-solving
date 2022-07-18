a297a293875dddce7028640f41fc42876f851f2f
kamlesh.vishwakarma <kamlesh.vishwakarma@rakuten.com>
2022-07-18 21:42:53 +0530

    TwoSum+AddTwoNumbers

---
+++ src/main/java/com/kk/medium/AddTwoNumbers.java
@@ -0,0 +1,76 @@
+package com.kk.medium;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Example 1:
+ * <p>
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * <p>
+ * Example 2:
+ *<p>
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * <p>
+ * Example 3:
+ *<p>
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ */
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode listNode = addTwoNumbers(l1, l2);
+        List<Integer> list = new ArrayList<>();
+        while(listNode != null){
+            list.add(listNode.val);
+            listNode = listNode.next;
+        }
+        System.out.println(list);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode current = result;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry !=0){
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum/10;
+            current = current.next = new ListNode(sum % 10);
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        return result.next;
+    }
+}
