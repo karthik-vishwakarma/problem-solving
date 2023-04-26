c5484e8dde152c9677755cad97ee8b6fded7057e
Kamlesh Vishwakarma <kvishwa100@gmail.com>
2023-04-26 23:56:43 +0530

    SubArrayWithGivenSum : doc

--- src/main/java/com/kk/array/SubArrayWithGivenSum.java
+++ src/main/java/com/kk/array/SubArrayWithGivenSum.java
@@ -2,28 +2,7 @@ package com.kk.array;
 
 import java.util.ArrayList;
 
-/**
- * Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
- * <p>
- * In case of multiple subarrays, return the subarray which comes first on moving from left to right.
- * <p>
- * Input:
- * N = 5, S = 12
- * A[] = {1,2,3,7,5}
- * Output: 2 4
- * Explanation: The sum of elements
- * from 2nd position to 4th position
- * is 12.
- * <p>
- * Input:
- * N = 10, S = 15
- * A[] = {1,2,3,4,5,6,7,8,9,10}
- * Output: 1 5
- * Explanation: The sum of elements
- * from 1st position to 5th position
- * is 15.
- */
-public class SubarrayWithGivenSum {
+public class SubArrayWithGivenSum {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 7, 5};
