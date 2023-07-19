afc7e6a2dec7c9bd20212d171b22c588edc93cff
Kamlesh Vishwakarma <kvishwa100@gmail.com>
2023-07-19 20:41:44 +0530

    SeparateStringAccordingToCount

--- src/main/java/com/kk/string/SeparateStringAccordingToCount.java
+++ src/main/java/com/kk/string/SeparateStringAccordingToCount.java
@@ -8,10 +8,32 @@ import java.util.stream.Collectors;
 
 public class SeparateStringAccordingToCount {
     public static void main(String[] args) {
-        String s1 = "10101010454";
+        String s1 = "101010104541";
         // Output: result="544111100000";
-        List<Map.Entry<Character, Long>> entries = s1.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toList());
+        seprateString(s1);
+        System.out.println();
+        System.out.println(arrangeSequenceString(s1));
+    }
 
+    public static String arrangeSequenceString(String str) {
+        StringBuilder builder = new StringBuilder();
+        str.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new))
+                .forEach((c, count) -> {
+                    while (count > 0) {
+                        builder.append(c);
+                        count--;
+                    }
+                });
+        return builder.toString();
+    }
+
+    private static void seprateString(String s1) {
+        List<Map.Entry<Character, Long>> entries = s1.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toList());
         for (Map.Entry<Character, Long> entry : entries) {
             Long value = entry.getValue();
             for (int i = 0; i < value; i++) {
